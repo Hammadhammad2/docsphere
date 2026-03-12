81c1983dfe685202239de4e9de4c470655d11f7a
Hammad Naeem <hammad.kinectro@gmail.com>
2026-03-12 12:53:14 +0500

    refactor: Rename Membership model to UserOrganization and update relationships in organization models

--- organizations/models.py
+++ organizations/models.py
@@ -13,17 +13,21 @@ class Organization(TimeStampedModel):
     description = models.TextField(blank=True, default='')
     name = models.CharField(max_length=255, unique=True)
 
-    members = models.ManyToManyField('users.User', through='organizations.Membership', related_name='organizations')
+    members = models.ManyToManyField(
+        'users.User', through='organizations.UserOrganization', related_name='organizations'
+    )
 
     def __str__(self):
         return self.name
 
 
-class Membership(TimeStampedModel):
+class UserOrganization(TimeStampedModel):
     role = models.CharField(max_length=20, choices=Role.choices, default=Role.MEMBER)
 
-    organization = models.ForeignKey('organizations.Organization', on_delete=models.CASCADE, related_name='memberships')
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='memberships')
+    organization = models.ForeignKey(
+        'organizations.Organization', on_delete=models.CASCADE, related_name='user_organizations'
+    )
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='user_organizations')
 
     class Meta:
         constraints = [models.UniqueConstraint(fields=['user', 'organization'], name='unique_user_organization')]
