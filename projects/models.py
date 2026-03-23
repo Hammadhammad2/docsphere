ea3404e85b394b74ab504a0416931ef642d5d83f
Hammad Naeem <hammad.kinectro@gmail.com>
2026-03-23 17:26:18 +0500

    refactor: Update ForeignKey reference in Project model for clarity
    - Changed the ForeignKey reference for the organization field in the Project model to use a string reference for better clarity and to avoid import issues.

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
-from organizations.models import Organization
-
 
 class Project(TimeStampedModel):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, default="")
-    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    organization = models.ForeignKey("organizations.Organization", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
